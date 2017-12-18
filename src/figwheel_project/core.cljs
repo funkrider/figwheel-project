ea65eae46070f6afb87dd2df98b3f240ec437a58
funkrider <ian_davies@hotmail.com>
2017-12-18 15:49:21 -0500

    Update to use om-next read and mutate for the reconciler. Apply IQuery protocol to counter object to retrieve state. Mutate function defines custom transition called increment. It is used to modify state.

--- src/figwheel_project/core.cljs
+++ src/figwheel_project/core.cljs
@@ -4,8 +4,6 @@
             [om.dom :as dom]
             [figwheel-project.config :as config]))
 
-(def app-state (atom {:count 0}))
-
 (defui Country-list
   Object
   (render [this]
@@ -15,6 +13,38 @@
 
 (def country-list (om/factory Country-list))
 
-(js/ReactDOM.render
-  (country-list {:FIPS config/FIPS})
-  (gdom/getElement "app"))
+
+(def app-state (atom {:count 0}))
+
+(defn read [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      {:value :not-found})))
+
+(defn mutate [{:keys [state] :as env} key params]
+  (if (= 'increment key)
+    {:value {:keys [:count]}
+     :action #(swap! state update-in [:count] inc)}
+    {:value :not-found}))
+
+(defui Counter
+  static om/IQuery
+  (query [this] [:count]) ;; Defines location of data in pull syntax
+  Object
+  (render [this]
+    (let [{:keys [count]} (om/props this)]
+      (dom/div nil
+        (dom/span nil (str "Count: " count))
+        (dom/button
+          #js {:onClick
+               (fn [e] (om/transact! this '[(increment)]))}
+          "Click me!")))))
+
+(def reconciler
+  (om/reconciler
+    {:state app-state
+     :parser (om/parser {:read read :mutate mutate})}))
+
+(om/add-root! reconciler
+  Counter (gdom/getElement "app"))
